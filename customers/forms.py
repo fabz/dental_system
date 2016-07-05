f99051cad740d58e35c75e11f53c6d1318023c77
Fabz <fabian@ruma.co.id>
2016-07-05 14:16:26 +0700

    fix form

--- customers/forms.py
+++ customers/forms.py
@@ -1,20 +1,18 @@
-from datetime import date
-
 from django import forms
-from customers.models import CustomerType, Customer
+from customers.models import Customer
 
 FIELDS = ['customer_type', 'name', 'place_of_birth', 'date_of_birth', 'phone_number1', 'phone_number2', 'email', 'address']
 
 
-class CustomersForm(forms.Form):
-    
-     class Meta:
+class CustomersForm(forms.ModelForm):
+
+    class Meta:
         model = Customer
         fields = FIELDS
     #date_of_birth = forms.DateField(input_formats=['%d-%m-%Y'], initial=date.today().strftime('%d-%m-%Y'), error_messages={"invalid": "Format must be dd-mm-yyyy"})
 
 
-#class CustomersEditForm(forms.ModelForm):
+# class CustomersEditForm(forms.ModelForm):
 
 #    class Meta:
 #        model = Customer
@@ -25,4 +23,3 @@ class CustomersForm(forms.Form):
 #    def __init__(self, *args, **kwargs):
 #        kwargs.pop("customer", None)
 #        super(CustomersEditForm, self).__init__(*args, **kwargs)
-
