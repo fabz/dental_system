cb4db8d13c217aca57c9d8d039565117e5c320a8
Fabz <fabian@ruma.co.id>
2016-07-24 14:25:02 +0700

    new form trx

--- transactions/forms.py
+++ transactions/forms.py
@@ -3,10 +3,12 @@ from datetime import date
 from django import forms
 
 from customers.models import Customer
+from dentists.models import Dentists
 from transactions.models import Transactions
+from treatments.models import Treatments
 
 
-FIELDS = ['trx_date', 'customer', 'total_amount', 'dentist']
+FIELDS = ['trx_date', 'customer', 'dentist']
 
 
 class TrxNewForm(forms.ModelForm):
@@ -16,6 +18,8 @@ class TrxNewForm(forms.ModelForm):
         fields = FIELDS
 
     customer = forms.ModelChoiceField(queryset=Customer.objects.all())
+    dentist = forms.ModelChoiceField(queryset=Dentists.objects.all())
+    Treatments = forms.ModelMultipleChoiceField(queryset=Treatments.objects.all(), widget=forms.CheckboxSelectMultiple())
 
     def __init__(self, *args, **kwargs):
         super(TrxNewForm, self).__init__(*args, **kwargs)
